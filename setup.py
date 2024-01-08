7d837fc207a95791934ea5ad6d4d75694c735515
Gerardo Madera <gerardomadera2103@gmail.com>
2024-01-08 16:16:45 -0700

    Adding the Cookiecutter Data Science project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Cookiecutter Data Science project structure',
+    author='Gerardo Madera',
+    license='MIT',
+)
